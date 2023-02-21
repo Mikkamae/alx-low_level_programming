6fb6f64bdf10aaf0f730368550ec2d90e4b14eb7
Mikkamae <hadassahamaefunah@gmail.com>
2023-02-21 12:27:12 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - A program that uses puts function to print a new line
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
